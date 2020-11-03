d8b8de82bd4fd48e97026561e90a5a0ad49f22cb
Lilehopho <72720925+Lilehopho@users.noreply.github.com>
2020-11-03 03:11:17 -0800

    Update slots.js

--- commands/slots.js
+++ commands/slots.js
@@ -1,3 +1,34 @@
+const symbols = ['coin', 'dollar', 'money_with_wings', 'moneybag'];
+const roll = [];
+const animation = [];
+
+function prob(symbol, value) {
+	this.symbol = symbol;
+	this.value = value;
+}
+
+function generateRoll() {
+	for (let i = 0; i < 3; i++) {
+		const number = getRandomNumber(101);
+		if (number <= 49) {
+			roll.push(new prob('coin', 1));
+		}
+		else if (number >= 50 && number <= 84) {
+			roll.push(new prob('dollar', 2));
+		}
+		else if (number >= 85 && number <= 94) {
+			roll.push(new prob('money_with_wings', 3));
+		}
+		else if (number >= 95) {
+			roll.push(new prob('moneybag', 4));
+		}
+	}
+}
+function cycleSlotMachine() {
+	while (true ) {
+		
+	}
+}
 function getRandomNumber(max) {
 	return Math.floor(Math.random() * Math.floor(max));
 }
@@ -9,7 +40,7 @@ module.exports = {
 	description: 'Casino game slots',
 	usage: '',
 	execute(message) {
-		const number = getRandomNumber(101);
+		generateRoll();
 
 		const Embed = new Discord.MessageEmbed()
 			.setColor('#FF763B')
